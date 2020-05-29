5447dd3857a6892ddd8e6095dba6240db91f2ca6
Eugene Kim <UserLogin@Eugenes-MacBook-Pro.local>
2020-05-29 18:56:04 -0500

    Updating project yet again

--- project/scripts/script.js
+++ project/scripts/script.js
@@ -50,6 +50,7 @@ const goPage5_1 = () => {
   document.querySelector('#page4').style.display = "none";
   document.querySelector('#page5_1').style.display = "block";
   document.querySelector('#page5_1_1').style.display = "none";
+  document.querySelector('#page5_1_2').style.display = "none";
   document.querySelector('#pagewrong_1').style.display = "none";
 };
 
@@ -63,21 +64,24 @@ const goPage5_1_1 = () => {
   document.querySelector('#page4').style.display = "none";
   document.querySelector('#page5_1').style.display = "none";
   document.querySelector('#page5_1_1').style.display = "block";
+  document.querySelector('#page5_1_2').style.display = "none";
   document.querySelector('#pagewrong_1').style.display = "none";
 };
 
 document.querySelector('#right_1').onclick = goPage5_1_1;
 
-const goPage5_2 = () => {
+const goPage5_1_2 = () => {
   document.querySelector('#page1').style.display = "none";
   document.querySelector('#page2').style.display = "none";
   document.querySelector('#page3').style.display = "none";
   document.querySelector('#page4').style.display = "none";
-  document.querySelector('#page5_2').style.display = "block";
+  document.querySelector('#page5_1').style.display = "none";
+  document.querySelector('#page5_1_1').style.display = "none";
+  document.querySelector('#page5_1_2').style.display = "block";
+  document.querySelector('#pagewrong_1').style.display = "none";
 };
 
-document.querySelector('#redpill').onclick = goPage5_2;
-document.querySelector('#tryagain_2').onclick = goPage5_2;
+document.querySelector('#right_1_1').onclick = goPage5_1_2;
 
 const wrong_1 = () => {
   document.querySelector('#page1').style.display = "none";
@@ -86,23 +90,64 @@ const wrong_1 = () => {
   document.querySelector('#page4').style.display = "none";
   document.querySelector('#page5_1').style.display = "none";
   document.querySelector('#page5_1_1').style.display = "none";
-  document.querySelector('#page5_2').style.display = "none";
+  document.querySelector('#page5_1_2').style.display = "none";
   document.querySelector('#pagewrong_1').style.display = "block";
 };
 
 document.querySelector('#wrong_12').onclick = wrong_1;
 document.querySelector('#wrong_13').onclick = wrong_1;
-document.querySelector('#wrong_22').onclick = wrong_1;
-document.querySelector('#wrong_23').onclick = wrong_1;
+document.querySelector('#wrong_14').onclick = wrong_1;
+document.querySelector('#wrong_15').onclick = wrong_1;
+document.querySelector('#wrong_16').onclick = wrong_1;
+document.querySelector('#wrong_17').onclick = wrong_1;
+
+const goPage5_2 = () => {
+  document.querySelector('#page4').style.display = "none";
+  document.querySelector('#page5_2').style.display = "block";
+  document.querySelector('#pagewrong_2').style.display = "none";
+};
+
+document.querySelector('#redpill').onclick = goPage5_2;
+document.querySelector('#tryagain_2').onclick = goPage5_2;
+
+const goPage5_2_1 = () => {
+  document.querySelector('#page5_2').style.display = "none";
+  document.querySelector('#page5_2_1').style.display = "block";
+};
+
+document.querySelector('#right_2').onclick = goPage5_2_1;
+
+const goPage5_2_2 = () => {
+  document.querySelector('#page5_2').style.display = "none";
+  document.querySelector('#page5_2_1').style.display = "none";
+  document.querySelector('#page5_2_2').style.display = "block";
+};
+
+document.querySelector('#right_2_1').onclick = goPage5_2_2;
 
 const wrong_2 = () => {
   document.querySelector('#page1').style.display = "none";
   document.querySelector('#page2').style.display = "none";
   document.querySelector('#page3').style.display = "none";
   document.querySelector('#page4').style.display = "none";
-  document.querySelector('#page5_1').style.display = "none";
   document.querySelector('#page5_2').style.display = "none";
+  document.querySelector('#page5_2_1').style.display = "none";
+  document.querySelector('#page5_2_2').style.display = "none";
   document.querySelector('#pagewrong_2').style.display = "block";
 };
 
-document.querySelector('.wrong_2').onclick = wrong_2;
+document.querySelector('#wrong_22').onclick = wrong_2;
+document.querySelector('#wrong_23').onclick = wrong_2;
+document.querySelector('#wrong_24').onclick = wrong_2;
+document.querySelector('#wrong_25').onclick = wrong_2;
+document.querySelector('#wrong_26').onclick = wrong_2;
+document.querySelector('#wrong_27').onclick = wrong_2;
+
+const goPage6 = () => {
+  document.querySelector('#page5_1_2').style.display = "none";
+  document.querySelector('#page5_2_2').style.display = "none";
+  document.querySelector('#page6').style.display = "block";
+};
+
+document.querySelector('#right_1_2').onclick = goPage6;
+document.querySelector('#right_2_2').onclick = goPage6;

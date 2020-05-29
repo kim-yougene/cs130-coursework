e768e4a63fa9dfecfe24c0fdfa4d3768ee7f1231
Eugene Kim <UserLogin@Eugenes-MacBook-Pro.local>
2020-05-29 17:28:07 -0500

    Updating project

--- project/scripts/script.js
+++ project/scripts/script.js
@@ -43,97 +43,66 @@ const goPage4 = () => {
 
 document.querySelector('#door').onclick = goPage4;
 
+const goPage5_1 = () => {
+  document.querySelector('#page1').style.display = "none";
+  document.querySelector('#page2').style.display = "none";
+  document.querySelector('#page3').style.display = "none";
+  document.querySelector('#page4').style.display = "none";
+  document.querySelector('#page5_1').style.display = "block";
+  document.querySelector('#page5_1_1').style.display = "none";
+  document.querySelector('#pagewrong_1').style.display = "none";
+};
 
-// // var pages = ["NewSite.html", "Intro.html", "tableofcontents.html", "ScarcityOfTime.html" ]
-// /* 0 = lights off 1 = lights on */
-// var lightcheck = 0
-// //
-// // /* This next part is our checker to see exactly what page we are on */
-// //
-// // for (counter = 0; counter < pages.length; counter += 1) {
-// //     var path = window.location.pathname;
-// //     var page = path.split("/").pop();
-// //     if (page === pages[counter]) {
-// //         pagebefore = pages[counter - 1];
-// //         pageafter = pages[counter + 1];
-// //     }
-// //
-// // }
-// //
-// //
-// //
-// //
-// //
-// // setTimeout(function() {
-// //   $('#author').fadeOut('fast');
-// // }, 5000);
-// //
-// // /* Bot Menu Functions */
-// // //
-// // // function homebutton() {
-// // //     document.getElementsByClassName("goHome").className = "animated fadeOut";
-// // //     setTimeout(home, 300);
-// // // }
-// // //
-// // // function home() {
-// // //     document.getElementsByClassName("goHome").className = "animated fadeIn";
-// // //     window.location = "newsite.html";
-// // // }
-// // //
-//     /* First check if lights are on/off , then turn off/turn on, respectively */
-// function onoffcheck() {
-//     if (lightcheck === 1) {
-//         return true;
-//     }
-//     else {
-//         return false
-//     }
-// }
-// //
-// // var iconnames = ["leftarrowicon", "rightarrowicon", "houseicon", "flashlighticon"]
-// //
-// //
-// function flashlightbutton() {
-//     document.getElementsByClassName("flashlight")
-//     if (onoffcheck() === true) {
-//
-//         document.body.style.backgroundColor = "black";
-//         document.body.color = "white";
-//         lightcheck = 0;
-//
-//         }
-//
-//     else if (onoffcheck() === false) {
-//         document.body.style.backgroundColor = "white";
-//         document.body.color = "black";
-//         lightcheck = 1;
-//
-//         }
-//     }
-//
-//
-// //
-// //
-// // /* Previous/Next Page buttons logic: have an array with all page names, then check via indexing what page I'm on,
-// //     then add 1 to whichever index position I'm at, and finally direct me to that page with indexvalue + 1 */
-// // //
-// // // function prevpagebutton() {
-// // //     document.getElementById("leftarrowicon").className = "animated fadeOut pull-left";
-// // //     setTimeout(prevpage, 600);
-// // // }
-// // //
-// // // function prevpage() {
-// // //     document.getElementById("leftarrowicon").className = "animated fadeIn pull-left";
-// // //     window.location = pagebefore;
-// // // }
-// // //
-// // // function nextpagebutton() {
-// // //     document.getElementById("rightarrowicon").className = "animated fadeOut pull-right";
-// // //     setTimeout(nextpage, 600);
-// // // }
-// // //
-// // // function nextpage() {
-// // //     document.getElementById("rightarrowicon").className = "animated fadeIn pull-right";
-// // //     window.location = pageafter;
-// // //
-// // // }
+document.querySelector('#bluepill').onclick = goPage5_1;
+document.querySelector('#tryagain_1').onclick = goPage5_1;
+
+const goPage5_1_1 = () => {
+  document.querySelector('#page1').style.display = "none";
+  document.querySelector('#page2').style.display = "none";
+  document.querySelector('#page3').style.display = "none";
+  document.querySelector('#page4').style.display = "none";
+  document.querySelector('#page5_1').style.display = "none";
+  document.querySelector('#page5_1_1').style.display = "block";
+  document.querySelector('#pagewrong_1').style.display = "none";
+};
+
+document.querySelector('#right_1').onclick = goPage5_1_1;
+
+const goPage5_2 = () => {
+  document.querySelector('#page1').style.display = "none";
+  document.querySelector('#page2').style.display = "none";
+  document.querySelector('#page3').style.display = "none";
+  document.querySelector('#page4').style.display = "none";
+  document.querySelector('#page5_2').style.display = "block";
+};
+
+document.querySelector('#redpill').onclick = goPage5_2;
+document.querySelector('#tryagain_2').onclick = goPage5_2;
+
+const wrong_1 = () => {
+  document.querySelector('#page1').style.display = "none";
+  document.querySelector('#page2').style.display = "none";
+  document.querySelector('#page3').style.display = "none";
+  document.querySelector('#page4').style.display = "none";
+  document.querySelector('#page5_1').style.display = "none";
+  document.querySelector('#page5_1_1').style.display = "none";
+  document.querySelector('#page5_2').style.display = "none";
+  document.querySelector('#pagewrong_1').style.display = "block";
+};
+
+document.querySelector('#wrong_12').onclick = wrong_1;
+document.querySelector('#wrong_13').onclick = wrong_1;
+document.querySelector('#wrong_22').onclick = wrong_1;
+document.querySelector('#wrong_23').onclick = wrong_1;
+
+const wrong_2 = () => {
+  document.querySelector('#page1').style.display = "none";
+  document.querySelector('#page2').style.display = "none";
+  document.querySelector('#page3').style.display = "none";
+  document.querySelector('#page4').style.display = "none";
+  document.querySelector('#page5_1').style.display = "none";
+  document.querySelector('#page5_2').style.display = "none";
+  document.querySelector('#pagewrong_2').style.display = "block";
+};
+
+document.querySelector('.wrong_2').onclick = wrong_2;
